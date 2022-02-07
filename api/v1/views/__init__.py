ee5d10c04b4fadd67234785779b65065e24d0053
enzonjagi <amosndungo@gmail.com>
2022-02-07 23:01:47 +0300

    API endpoints for the various models(Places, Users, States, Cities, Amenities)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 '''Setting up the flask Blueprint'''
 from flask import Blueprint
-from api.v1.views.index import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
+from api.v1.views.places import *  # noqa
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
