31a41a73f7cd2bf214f8d4ff877dbebab7ccab35
enzonjagi <amosndungo@gmail.com>
2022-02-15 10:16:56 +0300

    API changes but still not workign parts

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-'''Flask App Blueprint Setup'''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from models import storage
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response, render_template, url_for
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+from models import storage
 import os
+from werkzeug.exceptions import HTTPException
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
+swagger = Swagger(app)
+
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -25,6 +42,4 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
